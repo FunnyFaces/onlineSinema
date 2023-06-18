ddde4ad74f966e00d4e6ebc64651e4047f465514
FunnyFaces <f.nerubenko@gmail.com>
2023-06-18 17:38:39 +0300

    In class ViewerStatistics, removed List<Viewer viewerList

--- src/main/java/org/onlinesinema/ViewerStatistics.java
+++ src/main/java/org/onlinesinema/ViewerStatistics.java
@@ -3,12 +3,12 @@ package org.onlinesinema;
 import java.util.List;
 
 public class ViewerStatistics {
-    private List<Viewer> viewerList;
+
     public static double averageAge(List<Viewer> viewerList) {
-       int sum = 0;
-       for (Viewer viewer : viewerList) {
-           sum += viewer.getAge();
-       }
-       return (double) sum / viewerList.size();
+        int sum = 0;
+        for (Viewer viewer : viewerList) {
+            sum += viewer.getAge();
+        }
+        return (double) sum / viewerList.size();
     }
 }
